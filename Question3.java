6046658915ee6242806d5e76d9112a455e31a829
Jovin <s10243666@connect.np.edu.sg>
2023-04-19 23:07:39 +0800

    commit q3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,11 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter an integer: ");
+
+    int num = in.nextInt();
+
+    int newNum = num * num;
+    System.out.println("Result: " + newNum);
   }
 }
